cb645ab8f42072a32e2bc5235e4076c8761aa5b8
Tillmann Weimer <tillmann1970@gmail.com>
2019-06-13 17:10:20 +0200

    try to establish link to the files

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+
+//mislyckat försök att exportera postype/taxonomy och ACF
+
+// $puppy_includes = array (
+//     //includes the custom post type file into the widget
+//     '/cpt.pp_dog.php',
+//     //includes the pp_dog_taxonomy into the widget
+//     '/cpt.pp_dog_taxonomy.php',
+//     //includes the custom fields into the widget
+//     //'/acf.dog_details.php'
+
+// );
+
+// foreach( $puppy_includes as $file ) {
+//     $filepath = locate_template('inc' . $file);
+//     if ( ! $filepath) {
+//         trigger_error( sprintf( 'Error locating /inc%s for inclusion', $file), E_USER_ERROR );
+//     }
+//     require_once $filepath;
+// }
+
+
+
+//require_once 'inc/acf.dog_details.php';
+//require_once 'inc/cpt.pp_dog_taxonomy.php';
+//require_once 'inc/cpt.pp_dog.php_once';
